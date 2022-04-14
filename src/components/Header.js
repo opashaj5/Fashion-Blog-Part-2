73dc82d69e639ffd1c8949a7d698cdcbb15d8ea0
opashaj5 <opashaj5@yahoo.com>
2022-04-13 22:00:34 -0500

    moved MainNav and added styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import MainNav from "./MainNav";
 export default function Header() {
     return (
         <header>
-            <MainNav />
             <h1>Sartre's List</h1>
             <h2>Better-Dressed People</h2>
+            <MainNav />
         </header>
     )
 }
